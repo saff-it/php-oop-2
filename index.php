392bd9fecc8817adc86808d6c71fb9780a993dd9
saff-it <saff_it@ymail.com>
2022-09-05 17:07:53 +0200

    product object created

--- index.php
+++ index.php
@@ -1,3 +1,51 @@
+<?php 
+
+    class Product{
+        protected $name;
+        protected $price;
+        protected $brand;
+        protected $kindOfAnimal;
+
+        function __construct($_name, $_price, $_brand, $_kindOfAnimal){
+            
+        }
+
+        public function getName(){
+            return $this->name;
+        }
+
+        public function setName($_name){
+            $this->name = $_name;
+        }
+
+        public function getPrice(){
+            return $this->price;
+        }
+
+        public function setPrice($_price){
+            $this->price = $_price;
+        }
+
+        public function getBrand(){
+            return $this->brand;
+        }
+
+        public function setBrand($brand){
+            $this->brand = $brand;
+        }
+
+        public function getkindOfAnimal(){
+            return $this->kindOfAnimal;
+        }
+
+        public function setkindOfAnimal($_kindOfAnimal){
+            $this->kindOfAnimal = $_kindOfAnimal;
+        }
+
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
